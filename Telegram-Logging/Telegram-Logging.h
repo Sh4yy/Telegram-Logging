bc0512b0cdf37d417bb2d499a92a5d68af4bde25
Shayan <shayan@shayans-mbp.fios-router.home>
2016-11-27 19:01:22 -0500

    Initial Commit

---
+++ Telegram-Logging/Telegram-Logging.h
@@ -0,0 +1,19 @@
+//
+//  Telegram-Logging.h
+//  Telegram-Logging
+//
+//  Created by Shayan on 11/27/16.
+//  Copyright © 2016 Shayan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Telegram-Logging.
+FOUNDATION_EXPORT double Telegram_LoggingVersionNumber;
+
+//! Project version string for Telegram-Logging.
+FOUNDATION_EXPORT const unsigned char Telegram_LoggingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Telegram_Logging/PublicHeader.h>
+
+
