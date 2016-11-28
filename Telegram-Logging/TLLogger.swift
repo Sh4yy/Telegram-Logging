d178283e1d1b955af2c71722d175acca54a49589
Sh4yy <shayan@shayans-mbp.fios-router.home>
2016-11-28 17:40:29 -0500

    Added Location to New Download Messages.

--- Telegram-Logging/TLLogger.swift
+++ Telegram-Logging/TLLogger.swift
@@ -7,21 +7,20 @@
 
 import Foundation
 
-class TLLogger {
+public class TLLogger {
     
     private var api_key : String!
     private var api_url : String!
     private var chat_id : String!
     private var debugger = false
-    private var didSetup = false
     
-    init(Debugging : Bool) {
+    public init(Debugging : Bool) {
         self.api_key = ""
         self.api_url = ""
         self.debugger = Debugging
     }
     
-    func setup(BOT_KEY : String, CHAT_ID : String) {
+    public func setup(BOT_KEY : String, CHAT_ID : String) {
         if !BOT_KEY.isEmpty {
             self.api_key = BOT_KEY
             self.chat_id = CHAT_ID
@@ -34,10 +33,12 @@ class TLLogger {
                 if Error == nil {
                     do {
                         if let json = try JSONSerialization.jsonObject(with: Data!, options: .allowFragments) as? [String:AnyObject] {
-                            print(json)
-                            if let result = json["ok"] as? NSNumber, result == 1 {
-                                self.didSetup = true
-                                print("didSetup")
+                            
+                            if let ok = json["ok"] as? NSNumber, ok == 1, let result = json["result"] as? Dictionary<String,AnyObject>{
+                                
+                                if let name = result["username"] as? String {
+                                print("TLLogger Has been activated. \n Bot's username: \(name)")
+                                }
                             }
                         }
                     } catch {
@@ -51,61 +52,39 @@ class TLLogger {
         }
     }
     
-    func log(_ text : String){
+    public func log(_ text : String){
         if (debugger) {
         sendMessage("🤖 \(text)")
         }
     }
     
     private func sendMessage(_ text : String){
-        if self.didSetup {
             let path = "/sendMessage?chat_id=\(self.chat_id!)&text=\(text)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
             sendRequest(path: path!)
-        }
     }
     
-    func reportNewDownload(_ reportLocation : Bool = false){
+    public func reportNewDownload(_ reportLocation : Bool = false, _ text : String = "🎈 New Download"){
         if UserDefaults.standard.value(forKey: "TLLDidOpen") == nil {
-            if self.didSetup {
                 if reportLocation {
                     getLocation()
                 } else {
-                UserDefaults.standard.set(true, forKey: "TLLDidOpen")
-                sendMessage("🎈 New Download")
-                }
-            }
-        }
-    }
-    
-    private func reportNewDownload(_ text : String){
-        if UserDefaults.standard.value(forKey: "TLLDidOpen") == nil {
-            if self.didSetup {
                 UserDefaults.standard.set(true, forKey: "TLLDidOpen")
                 sendMessage(text)
             }
         }
     }
     
-    private func sendRequest(path : String) {
-        let url = URL(string: self.api_url + path as String)
-        let request = URLRequest(url: url!)
-        let session = URLSession.shared.dataTask(with: request)
-        session.resume()
-        
-    }
-
     private func getLocation() {
         let url = URL(string : "http://ip-api.com/json".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)
         print(url!)
         let request = URLRequest(url : url!)
         let session = URLSession.shared.dataTask(with : request, completionHandler : {
         (Data, URLResponse, Error) in
-            print(Data!)
         if Error == nil {
             do {
                 if let json = try JSONSerialization.jsonObject(with  : Data!, options: .allowFragments) as? [String : AnyObject] {
                     if let city = json["city"] as? String, let country = json["country"] as? String {
-                        self.reportNewDownload("🎈 New Download From \(country), \(city)")
+                        self.reportNewDownload(false,"🎈 New Download From \(country), \(city)")
                     }
                 }
             } catch {
@@ -116,5 +95,13 @@ class TLLogger {
             }
         })
         session.resume()
+    }
+    
+    private func sendRequest(path : String) {
+        let url = URL(string: self.api_url + path as String)
+        let request = URLRequest(url: url!)
+        let session = URLSession.shared.dataTask(with: request)
+        session.resume()
+        
     }
 }
